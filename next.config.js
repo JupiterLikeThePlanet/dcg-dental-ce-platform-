a2c9e9e7b8caed7c92a7b52eca962b1092c6cedb
Jupiter <jupiterbaudot@gmail.com>
2026-01-15 12:12:57 -0800

    upstream: add unoptimized: true for dev

--- next.config.js
+++ next.config.js
@@ -1,12 +1,22 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'images.unsplash.com',
+//       },
+//     ],
+//     minimumCacheTTL: 0,
+//   },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
+    unoptimized: true,
   },
 };
 

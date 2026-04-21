f542f13dc7fb9fb1ffcb9c28f172818123ca9397
Jupiter <jupiterbaudot@gmail.com>
2026-04-21 12:03:01 -0700

    Add reset password foundation

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -134,6 +134,15 @@ export default async function DashboardPage() {
             </svg>
             Browse All Classes
           </Link>
+          <Link
+            href="/change-password"
+            className="flex-1 p-4 bg-gray-100 text-gray-700 rounded-xl hover:bg-gray-200 transition-colors flex items-center justify-center gap-3 font-semibold border border-gray-200"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+            </svg>
+            Change Password
+          </Link>
         </div>
 
         {/* Submissions Section */}
